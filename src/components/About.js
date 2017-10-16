6354762d99e0f1da0a683012b9d5b901753b5644
Steven <steven@Stevens-MacBook-Pro.local>
2017-10-16 17:08:20 -0400

    about page with image

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Fire from "./Fire.JPG"
+// import Fire from "../../public/Fire.JPG"
 
 class About extends Component {
   render () {
     let {user} = this.props
     return(
       <div>
-        <img src={Fire} height="500" width="300"/>
+        <img src="./Fire.JPG" height="500" width="300"/>
         <p>{user.bio}</p>
       </div>
     )
