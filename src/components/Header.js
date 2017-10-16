7428075af3d3ab3d55a9f6863fa2aec374cef15d
Rebecca trachtenberg <rebecca.rose1836@yahoo.com>
2017-10-16 18:38:56 -0400

    working on it

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,22 @@ class Header extends Component {
   render () {
     let {user} = this.props
     return(
-        <a href={user.resume}>About</a>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo">Rebecca Trachtenberg</a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href={user.resume}>About</a></li>
+          </ul>
+          <ul class="side-nav" id="mobile-demo">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">Javascript</a></li>
+            <li><a href="mobile.html">Mobile</a></li>
+          </ul>
+        </div>
+      </nav>
+
     )
   }
 }
